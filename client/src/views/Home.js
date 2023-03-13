8189bd94140d77f965ada7165275e178295cbcc2
victor-val <vval26v@gmail.com>
2023-03-13 17:20:39 +0100

    get token in home

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import User from "../contexts/user";
 
 /**
@@ -7,13 +7,19 @@ import User from "../contexts/user";
  */
 const Home = () => {
   const user = useContext(User);
+  const token = JSON.parse(localStorage.getItem("token"));
+
+  useEffect(() => {
+    user.updateUser(true);
+  }, [token]);
 
   return (
     <main>
       <h1>Training notes</h1>
       {!user.signedIn && <p>Debe iniciar sesión o registrarse</p>}
       <nav className="secondary">
-        {user.signedIn && (
+        {user.signedIn && 
+        (
           <NavLink to="/notes">
             Ir a notas
           </NavLink>
