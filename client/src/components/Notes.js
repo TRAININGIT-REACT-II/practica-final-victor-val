3998e1055410124432e87e169bc096430fac4e95
victor-val <vval26v@gmail.com>
2023-03-13 13:25:58 +0100

    Add notes

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef, useState} from "react";
 import User from "../contexts/user";
 import useApi from "../hooks/useApi";
 
@@ -6,19 +6,66 @@ const Notes = () => {
     const user = useContext(User);
     const token = JSON.parse(localStorage.getItem("token"));
     let notesRequest = useApi("/api/notes", token, {}, false);
+    let notesInsertRequest = useApi("/api/notes", token, {}, false);
+    const addNotaRef = useRef();
+    const [enviarNota, setEnviarNota] = useState(false);
 
-    let notes;
-    if (notesRequest.data) {
-        notes = notesRequest.data;
-    }
+    const [notes, setNotes] = useState([]);
+    let nota;
 
     const fetchNotes = () => {
         notesRequest.perform();
     }
+    
+    useEffect(() => {
+        if(notesRequest.data){
+            const data = [...notesRequest.data];
+            notesRequest.data = null;
+            setNotes(data);
+        }
+    }, [notesRequest.data]);
+
+    useEffect(() => {
+        if(notesInsertRequest.data){        
+            if(enviarNota){
+                setEnviarNota(false);                
+                setNotes(
+                    [...notes,
+                    notesInsertRequest.data]
+                );
+                notesInsertRequest.data = null;
+            }
+          
+        }
+    }, [notesInsertRequest.data]);
 
     useEffect(() => {
         fetchNotes();
     }, [token]);
+
+    useEffect(() => {
+        if(enviarNota){
+            const nota = addNotaRef.current.value;       
+            notesInsertRequest.updateParams({
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title: nota,
+                    content: nota,
+                }),
+            });
+            addNotaRef.current.value = null;
+            notesInsertRequest.perform();
+        }
+    }, [enviarNota]);
+
+    const handleAddNota = () => {
+        nota = addNotaRef.current.value;
+        if(nota === '') return;
+        setEnviarNota(true);         
+    } 
 
     return (
         <div>
@@ -27,8 +74,13 @@ const Notes = () => {
             </h3>
 
             {notes && notes.map(note => (
-                <li>{note}</li>
+                <li>{note.title}</li>
             ))}
+            <div className="addNota">
+                <input ref={addNotaRef} type="text" placeholder='Nueva nota'></input>
+                <button onClick={handleAddNota}>Añadir nota</button>
+            </div>
+            
             
         </div>
     );
