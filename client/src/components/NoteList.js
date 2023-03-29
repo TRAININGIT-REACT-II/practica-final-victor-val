0f43c733587218984fb660427b7ee3597bcc27cc
victor-val <vval26v@gmail.com>
2023-03-29 10:28:47 +0200

    Boton ordenar notas por nombre

--- client/src/components/NoteList.js
+++ client/src/components/NoteList.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import NoteItem from './NoteItem';
+import {useState, useEffect} from "react";
 
 export function NoteList({notes, showDetail, handleEditNote, deleteNote}) {
+    const [notas, setNotas] = useState([]);
+
+    useEffect(() => {
+        setNotas(notes);
+    }, [notes]);
+
+    const handleOrder = () =>{
+        let notes = [...notas];
+        setNotas([]);
+        notes.sort((a,b) => a.title.localeCompare(b.title));
+        setNotas(notes);
+    }
     return (
-        <ul>
-            {notes && notes.map(note => (
-                <NoteItem note={note} showDetail={showDetail} handleEditNote={handleEditNote} deleteNote={deleteNote}/>
-            ))}
-        </ul>
+        <>
+            <button onClick={handleOrder}>Ordenar por title</button>
+            <ul>
+                {notas && notas.map(note => (
+                    <NoteItem note={note} showDetail={showDetail} handleEditNote={handleEditNote} deleteNote={deleteNote} />
+                ))}
+            </ul>
+        </>
     );
 }
